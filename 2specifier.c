889bc75d82d2b48e47b2181bed6cf9bc87f6c810
Tejas Chaudhari <tejaschaudhari1904@gmail.com>
2025-09-19 16:18:07 +0530

    Add files via upload

---
+++ 2specifier.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+
+    const int rollno = 11;
+    float per = 78.91;
+    char grade = 'A';
+
+    printf("Roll no.: %d \n",rollno);
+
+    printf("Grade: %c \n",grade);
+
+    printf("Percentage: %f \n",per);
+
+
+}
